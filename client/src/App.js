31c79f0912c7f2570a3c902663cb7081ea085220
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-11-19 23:33:21 -0500

    updating the working code

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache,createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-//Creating Http server
-
+// Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
-}); 
+});
 
-
-//Copied from mini project
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
@@ -49,6 +52,5 @@ function App() {
     </ApolloProvider>
   );
 }
-
 
 export default App;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,38 +1,47 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql `
-type User {
-    _id:ID!
+const typeDefs = gql`
+  type User {
+    _id: ID!
     username: String!
-    email: String!
+    email: String
     bookCount: Int
-    savedBooks: [Books]
-}
+    savedBooks: [Book]
+  }
 
-type Book {
+  type Book {
     bookId: ID!
     authors: [String]
     description: String
-    title: String
     image: String
     link: String
-}
+    title: String!
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User
-}
+  }
 
-type Query {
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
     me: User
-}
+  }
 
-type Mutation {
-    login(email:String! , password: String): Auth
+  type Mutation {
+    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookData:Book! ): User
+    saveBook(bookData: BookInput!): User
     removeBook(bookId: ID!): User
-}
+  }
 `;
 
 module.exports = typeDefs;
