aae8a07031269f7022b13faf7c76671bf445db39
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-11-19 23:45:47 -0500

    Established MongoDb connection in server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -27,6 +28,20 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksdb",
+ {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
+}
+);
+mongoose.connection.on('error', (err)=>{
+console.error(err);
+}
+)
 
 db.once('open', () => {
   app.listen(PORT, () => {
